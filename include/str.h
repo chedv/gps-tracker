e8e94c5a7dad298044d1eb5f1dee0c948acd9149
chedv001 <chernovdv001@gmail.com>
2020-04-07 14:41:06 +0300

    final impl based on the atmelavr platform

--- include/str.h
+++ include/str.h
@@ -3,11 +3,8 @@
 
 #include <Arduino.h>
 
-//-------------------------------------------------------------------------
-
-namespace str {
-
-//-------------------------------------------------------------------------
+namespace str 
+{
 
 bool empty(const char * str);
 
@@ -17,11 +14,7 @@ void substr(const char * str, char * buffer, uint8_t pos);
 
 void substr(const char * str, char * buffer, uint8_t left, uint8_t right);
 
-//-------------------------------------------------------------------------
-
 }; // namespace str
-
-//-------------------------------------------------------------------------
 
 inline bool str::empty(const char * str)
 {
@@ -32,7 +25,5 @@ inline uint8_t str::indexOf(const char * str, char chr)
 {
     return strchr(str, chr) - str;
 }
-
-//-------------------------------------------------------------------------
 
 #endif // _STR_H_
